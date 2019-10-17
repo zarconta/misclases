f5022bcb2c800dbf3e03b36de7274fbd440e1354
Javier Murcia <javiermurcia@hotmail.com>
2019-10-16 21:36:44 -0500

    segunda

--- src/Hello.php
+++ src/Hello.php
@@ -14,7 +14,7 @@ class Hello{
 		print 'Hello composer and github - Javier Murcia. lamus. <br>';
 		print ' primero guardo local,<br>';
 		print ' segundo relaciono con el github  de internet <br>';
-		print ' y tercero hago push y actualizo el github';
+		print ' y tercero hago push y actualizo el github<br>';
 		print '  por ultimo composer update y todas las paginas se cambian.';
 	}
 }
