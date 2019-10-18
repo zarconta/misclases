d60fd16782d070ec39f78bde3f417c0797689b96
Javier Murcia <javiermurcia@hotmail.com>
2019-10-18 17:04:30 -0500

    hice un cambio

--- src/Hello.php
+++ src/Hello.php
@@ -11,7 +11,7 @@ class Hello{
 	
 	function __construct()
 	{
-		print ' Volvi a usar el commit.Hello composer and github - Javier Murcia. lamus. <br>';
+		print ' Volvi a usar el commit solo actualizo mi git local y luego hago push.Hello composer and github - Javier Murcia. lamus. <br>';
 		print ' primero guardo local, con add<br>';
 		print ' segundo commit en el local<br>';
 		print ' tercero conecto con el githuh con un add<br>';
